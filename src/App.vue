07dc4a5c26f31d0489006149d3f1b5fa7396e4c8
Murilo-O-Ramos <murilooramos@hotmail.com>
2025-05-16 11:36:15 -0300

    Mercado fipp frontend

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <form-categoria msg="Cadastro de Categorias"/>
+</template>
+
+<script>
+import FormCategoria from './components/FormCategoria.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FormCategoria
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
